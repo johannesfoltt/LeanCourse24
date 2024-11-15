19f1403ff87c17fa5d1af6b94680bd8bfb5c40eb
Johannes Folttmann <johannesfolttmann@uni-bonn.de>
2024-11-15 13:07:50 +0100

    Hello

--- LeanCourse/Assignments/Assignment6.lean
+++ LeanCourse/Assignments/Assignment6.lean
@@ -105,7 +105,11 @@ instance : MulAction G (Subgroup G) := sorry
 Let's define the smallest equivalence relation on a type `X`. -/
 def myEquivalenceRelation (X : Type*) : Setoid X where
   r x y := x = y
-  iseqv := sorry -- Here you have to show that this is an equivalence.
+  iseqv := {
+    refl := fun x ↦ rfl
+    symm := by {intros x y h; rw [h]}
+    trans := by {intros x y z hxy hyz; rw [hxy, hyz]}
+  } -- Here you have to show that this is an equivalence.
                  -- If you click on the `sorry`, a lightbulb will appear to give the fields
 
 /- This simp-lemma will simplify `x ≈ y` to `x = y` in the lemma below. -/
@@ -122,8 +126,30 @@ Use `Quotient.sound` to prove that two elements of the quotient are equal.
 Use `Quotient.ind` to prove something for all elements of a quotient.
 You can use this using the induction tactic: `induction x using Quotient.ind; rename_i x`.
 -/
+
+lemma id_lift (X : Type*) :
+  letI := myEquivalenceRelation X; ∀ (a b : X), a ≈ b → id a = id b := by {
+    intros a b h
+    simp at h
+    assumption
+  }
+
 def quotient_equiv_subtype (X : Type*) :
-    Quotient (myEquivalenceRelation X) ≃ X := sorry
+    Quotient (myEquivalenceRelation X) ≃ X where
+      toFun := Quotient.lift id (id_lift X)
+      invFun := Quotient.mk (myEquivalenceRelation X)
+      left_inv := by {
+        intro x
+        induction x using Quotient.ind
+        rename_i x
+        apply Quotient.sound
+        simp
+      }
+      right_inv := by {
+        unfold Function.RightInverse
+        intro x
+        simp
+      }
 
 
 
@@ -137,12 +163,49 @@ precisely when one element is in the orbit of the other. -/
 def orbitOf (x : X) : Set X := range (fun g : G ↦ g • x)
 
 lemma orbitOf_eq_iff (x y : X) : orbitOf G x = orbitOf G y ↔ y ∈ orbitOf G x := by {
-  sorry
+  constructor
+  · intro h
+    rw [h]
+    use 1
+    simp
+  · intro h
+    ext a
+    constructor
+    · intro ha
+      obtain ⟨g, hy⟩ := h
+      obtain ⟨h, ha⟩ := ha
+      simp at hy ha
+      use (h * g⁻¹)
+      simp
+      calc (h * g⁻¹) • y = h • (g⁻¹ • y) := by {rw [mul_smul]}
+      _= h • x := by {rw [← hy]; simp}
+      _= a := by {exact ha}
+    · intro ha
+      obtain ⟨g, hy⟩ := h
+      obtain ⟨h, ha⟩ := ha
+      simp at hy ha
+      use (h * g)
+      simp
+      rw [mul_smul, hy, ha]
   }
 
 /- Define the stabilizer of an element `x` as the subgroup of elements
 `g ∈ G` that satisfy `g • x = x`. -/
-def stabilizerOf (x : X) : Subgroup G := sorry
+def stabilizerOf (x : X) : Subgroup G where
+  carrier := {g : G | g • x = x}
+  mul_mem' := by {
+    intros a b ha hb
+    simp
+    rw [mul_smul, hb, ha]
+  }
+  one_mem' := by simp
+  inv_mem' := by {
+    intros g hg
+    simp
+    simp at hg
+    nth_rewrite 1 [← hg]
+    simp
+  }
 
 -- This is a lemma that allows `simp` to simplify `x ≈ y` in the proof below.
 @[simp] theorem leftRel_iff {x y : G} {s : Subgroup G} :
@@ -154,6 +217,8 @@ def stabilizerOf (x : X) : Subgroup G := sorry
 Hint: Only define the forward map (as a separate definition),
 and use `Equiv.ofBijective` to get an equivalence.
 (Note that we are coercing `orbitOf G x` to a (sub)type in the right-hand side) -/
+--def orbit_map (x : X) := Set.codRestrict ...
+
 def orbit_stabilizer_theorem (x : X) : G ⧸ stabilizerOf G x ≃ orbitOf G x := by {
   sorry
   }
