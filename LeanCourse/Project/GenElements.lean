e3b2dd021d2330cf1c1a186b0186724d66c1489b
fohannesj <johannesfolttmann@gmail.com>
2025-01-16 14:41:14 +0100

    16.01

---
+++ LeanCourse/Project/GenElements.lean
@@ -0,0 +1,7 @@
+import Mathlib
+
+/- open namespaces that you use to shorten names and enable notation. -/
+open CategoryTheory ChosenFiniteProducts Closed MonoidalCategory monoidalOfHasFiniteProducts
+
+/- recommended whenever you define anything new. -/
+noncomputable section
